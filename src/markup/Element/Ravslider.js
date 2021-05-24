f3d431ac685a15130bbb8e023818354cdad3f8fa
github username <glennan20@gmail.com>
2021-05-24 11:34:18 -0400

    changed banner and fixed typo

--- src/markup/Element/Ravslider.js
+++ src/markup/Element/Ravslider.js
@@ -15,7 +15,7 @@ class Banner extends Component{
 						<div class="row">
 							<div class="col-lg-6 text-center">
 								<div class="content">
-									<h2 className="title">Voted <br/> Best <br/> CheesSteakes</h2>
+									<h2 className="title">Voted <br/> Best <br/> CheeseStakes</h2>
 									
 									<div className= "bnr-logo">
 										<img src ={require('./../../images/main-slider/slide5.png')}  alt="offer" />
