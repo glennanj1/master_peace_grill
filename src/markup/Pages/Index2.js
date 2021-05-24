e7704b7a4f17783e3c5355e31059900e99a0c467
github username <glennan20@gmail.com>
2021-05-24 11:37:26 -0400

    fixed link

--- src/markup/Pages/Index2.js
+++ src/markup/Pages/Index2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Banner from './../Element/Ravslider';
 
 import Header2 from './../Layout/Header2';
