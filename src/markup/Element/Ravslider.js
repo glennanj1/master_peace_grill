f56f9697930641d52cc613f14a77e3d6bf666f9f
github username <glennan20@gmail.com>
2021-06-15 15:22:02 -0400

    added cover style to background size

--- src/markup/Element/Ravslider.js
+++ src/markup/Element/Ravslider.js
@@ -9,25 +9,24 @@ class Banner extends Component{
 		return(
 			
 			<div className= "banner">	
-				<div className="section-full content-inner bnr-bg" style={{backgroundImage: "url(" + bnr + ")", backgroundSize: "100%"}}>
+				<div className="section-full content-inner bnr-bg" style={{backgroundImage: "url(" + bnr + ")", backgroundSize: "cover"}}>
 					<div className="container">
 						
 						<div class="row">
 							<div class="col-lg-6 text-center">
 								<div class="content">
-									<h2 className="title"> Voted Best <br/> CheeseSteaks</h2>
+									<h2 className="title">Best CheeseSteaks</h2>
 									
-									<div className= "bnr-logo">
+									{/* <div className= "bnr-logo">
 										<img src ={require('./../../images/main-slider/slide5.png')}  alt="offer" />
-									</div>	
+									</div>	 */}
 									
 									<p className="info">Conshohockens best sandwhich shop going on 7 years.</p>
 									
-									<a href={this.props.online} className="btn red"  data-target="#exampleModal">Order Online</a>
+									<a href={this.props.online} className="btn red" style={{fontSize: '34px'}} data-target="#exampleModal">Order Online</a>
 								</div>
 							</div>
 						</div>
-						
 					</div>
 				</div>
 			</div>
