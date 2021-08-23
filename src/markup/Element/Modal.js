28fb019b4b1a1ab8a24e788a83b3c02b35a5ca87
john glennan <glennan20@gmail.com>
2021-08-23 15:49:10 -0400

    client changes

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -28,7 +28,7 @@ export default class Modal extends React.Component {
             {this.state.isOpen ? (
                 <div className="__wrapper">
                     <div className="__child">
-                        <h4 className="__text">We will be closed Tuesday 08/24 to attend the Grateful Dead Concert</h4>
+                        <h4 className="__text">We will be closed Tuesday 08/24 to attend Dead & Co</h4>
                         <h4 className="__text">We will reopen on Wednesday 08/25</h4>
                         <h4 className="__text">Thank you for your understanding.</h4>
                         <button className="__button" onClick={this.toggleModal}>Close</button>
