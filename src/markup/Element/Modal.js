e296aa813252641d13493e7ccfbdbe1e3811b743
john glennan <glennan20@gmail.com>
2021-08-23 15:44:47 -0400

    changed modal to reflect closing on tuesday for client

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 export default class Modal extends React.Component {
 
     state = {
@@ -21,13 +22,16 @@ export default class Modal extends React.Component {
 
 
   render() {
+
+    const grateful = 'https://d3ddatyom1hv87.cloudfront.net/grateful.mp4'
     return (
         <div>
             {this.state.isOpen ? (
                 <div className="__wrapper">
                     <div className="__child">
-                        <h4 className="__text">We will be closed Saturday 08/21 to attend the Grateful Dead Concert</h4>
-                        <h4 className="__text">We will reopen on Sunday 08/22</h4>
+                        <h4 className="__text">We will be closed Tuesday 08/24 to attend the Grateful Dead Concert</h4>
+                        <h4 className="__text">We will reopen on Wednesday 08/25</h4>
+                        <h4 className="__text">Thank you for your understanding.</h4>
                         <button className="__button" onClick={this.toggleModal}>Close</button>
                     </div>
                 </div> ) : null}
