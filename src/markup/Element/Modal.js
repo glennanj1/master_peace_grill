e2f5bff5d711c2210f2bcf29bde87c65f6193d70
John Glennan <glennan20@gmail.com>
2021-09-05 09:37:51 -0400

    modal inline css and stylesheet update

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const background = 'https://static.wonderfulunion.net/groundctrl/clients/groundctrl/media/09/06/media/release_images/assets/large.szVfY7CoJkgiZNQVDE8VdNzuUyAVZIJ8HsohHt2g17I.jpg'
+
 export default class Modal extends React.Component {
 
     state = {
@@ -26,7 +28,7 @@ export default class Modal extends React.Component {
         <div>
             {this.state.isOpen ? (
                 <div className="__wrapper">
-                    <div className="__child">
+                    <div className="__child" style={{background: `url(${background})`, backgroundSize: '100vh', backgroundPosition: 'center'}}>
                         <h4 className="__text">We are Closed Sunday 09/05</h4>
                         <h4 className="__text">Have a great Labor Day</h4>
                         <h4 className="__text">See everyone on Tuesday 09/07</h4>
