c904eef1903a4b8b649810aefefc18763ff6a143
John Glennan <glennan20@gmail.com>
2021-09-18 09:09:43 -0400

    modal push

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -29,9 +29,9 @@ export default class Modal extends React.Component {
             {this.state.isOpen ? (
                 <div className="__wrapper">
                     <div className="__child" style={{background: `url(${background})`, backgroundSize: '100vh', backgroundPosition: 'center'}}>
-                        <h4 className="__text">We are Closed Sunday 09/05</h4>
-                        <h4 className="__text">Have a great Labor Day</h4>
-                        <h4 className="__text">See everyone on Tuesday 09/07</h4>
+                        <h4 className="__text">We are Closed Today 09/18</h4>
+                        <h4 className="__text">Reopening tomorrow 09/19</h4>
+                        <h4 className="__text">Sorry for the Inconvienence</h4>
                         <button className="__button" onClick={this.toggleModal}>Close</button>
                     </div>
                 </div> ) : null}      
