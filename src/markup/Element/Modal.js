fcb40aa57ba5d8ca66ea31e6bb7318026816863a
John Glennan <glennan20@gmail.com>
2021-09-18 10:33:18 -0400

    modal background

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -28,7 +28,7 @@ export default class Modal extends React.Component {
         <div>
             {this.state.isOpen ? (
                 <div className="__wrapper">
-                    <div className="__child" style={{background: `url(${background})`, backgroundSize: '100vh', backgroundPosition: 'center'}}>
+                    <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
                         <h4 className="__text">We are Closed Today 09/18</h4>
                         <h4 className="__text">Reopening tomorrow 09/19</h4>
                         <h4 className="__text">Sorry for the Inconvienence</h4>
