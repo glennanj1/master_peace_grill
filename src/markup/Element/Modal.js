336a3752d8d90d0b56e4749d06d2520fafecd35e
John Glennan <61764500+glennanj1@users.noreply.github.com>
2021-10-01 16:36:13 -0400

    fixing background for client

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -28,7 +28,8 @@ export default class Modal extends React.Component {
         <div>
             {this.state.isOpen ? (
                 <div className="__wrapper">
-                    <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                    {/* <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}> */}
+                    <div className="__child" style={{background: 'rgb(238,174,202)', background: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(181,43,43,1) 100%'), backgroundSize: 'cover', backgroundPosition: 'center'}}>
                         <h4 className="__text">We are Closing Early</h4>
                         <h4 className="__text">at 8PM Tonight</h4>
                         <h4 className="__text">Sorry for the Inconvienence</h4>
