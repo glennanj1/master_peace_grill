0f2401815cc0a47416f9505af31d75d911d4932e
glennanj1 <glennan20@gmail.com>
2021-10-21 00:59:43 -0400

    import for modal bg and confetti + setup changes per client request

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -1,4 +1,6 @@
+import confetti from "canvas-confetti";
 import React from "react";
+import steak from "../../images/steak_tray.jpg"
 
 // const background = 'https://media.giphy.com/media/K9i1AnJdj2kUfay0Rm/giphy.gif'
 
@@ -18,6 +20,16 @@ export default class Modal extends React.Component {
         }
     }
 
+    confettiLaunch = () => {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+            zIndex: 100000,
+          });
+    }
+
+
     componentDidMount() {
         this.toggleModal();
     }
@@ -29,11 +41,13 @@ export default class Modal extends React.Component {
             {this.state.isOpen ? (
                 <div className="__wrapper">
                     {/* <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}> */}
-                    <div className="__child" style={{background: 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(181,43,43,1) 100%)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
-                        <h4 className="__text">We are Closing Early</h4>
-                        <h4 className="__text">at 8PM Tonight</h4>
-                        <h4 className="__text">Sorry for the Inconvienence</h4>
-                        <h4 className="__text">Please order by 7:30PM</h4>
+                    <div className="__child" style={{borderRadius: '10px', backgroundImage: "url(" + steak + ")", backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                        <h4 className="__text">Vote</h4>
+                        <h4 className="__text">Master Peace Grill</h4>
+                        <h4 className="__text">For Best CheeseSteaks</h4>
+                        <button className="__button" onClick={this.confettiLaunch}><a style={{color: 'inherit'}} href="https://morethanthecurve.com/best-of-conshy/">Vote Here</a></button>
+                        {/* <h4 className="__text">OR</h4> */}
+                        <h5 className="__text">or</h5>
                         <button className="__button" onClick={this.toggleModal}>Close</button>
                     </div>
                 </div> ) : null}      
