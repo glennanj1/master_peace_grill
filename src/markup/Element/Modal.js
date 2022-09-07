fe78bed53f05768cc6d189a4554562efac3ec3af
glennanj1 <glennan20@gmail.com>
2022-09-07 11:29:17 -0400

    modal for more then the curve best of conshy

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -43,17 +43,20 @@ export default class Modal extends React.Component {
                     {/* <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}> */}
                     <div className="__child" style={{borderRadius: '10px', background: 'white'}}>
                         <div>
-                            <p style={{fontSize: '1em', padding: '25px'}}>
-                            Masterpeace Grill will be closed for Vacation 
-                            8/14 threw 8/22<span role="img" aria-label="peace">
-                            ✌️❤️🎼🎼🎼🎼
+                            <p style={{fontSize: '1.5em', padding: '20px'}}>
+                            Vote for Master Peace Grill for Best CheeseSteak!
+                            <span role="img" aria-label="peace">
+                            ✌️❤️
                             </span>
                             </p>
                         </div>
-                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://scontent-iad3-2.xx.fbcdn.net/v/t39.30808-6/298989073_5644966415533751_8313348424198655745_n.jpg?stp=cp1_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=lb6svY6Y84UAX8NIOhi&_nc_ht=scontent-iad3-2.xx&oh=00_AT_K28cHS668q5vi8PFvcWcAXDVOwUpquPW45IP3B5ZDQw&oe=62FC15AA)", backgroundSize: 'contain', backgroundPosition: 'center'}}>
+                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://storage.googleapis.com/burbcommunity-morethanthecurve/2022/09/best-of-finals-header-1.jpg)", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
                         </div>
-                        <div style={{height: '100px', width: '100px'}}>
-                            <button className="__button" onClick={this.toggleModal} style={{bottom: '5px', position: 'relative'}}>Close</button>
+                        <div style={{height: '30%', width: '100px', marginBottom: '10px'}}>
+                            <a href="https://morethanthecurve.com/best-of-conshy/" style={{textDecoration: 'none', color: 'white'}}><button className="__button" style={{bottom: '5px'}}>Vote Now</button></a>
+                        </div>
+                        <div style={{height: '30%', width: '100px'}}>
+                            <button className="__button" onClick={this.toggleModal} style={{top: '5px', position: 'relative'}}>Close</button>
                         </div>
                     </div>
                 </div> ) : null}      
