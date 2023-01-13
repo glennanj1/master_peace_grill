ac8a2ef3ac82ef079cfa17064108adc378785502
glennanj1 <glennan20@gmail.com>
2023-01-12 20:24:22 -0500

    modal push

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -44,13 +44,13 @@ export default class Modal extends React.Component {
                     <div className="__child" style={{borderRadius: '10px', background: 'white'}}>
                         <div>
                             <p style={{fontSize: '1.5em', padding: '20px'}}>
-                            We will be closed for Chirstmas Eve & Christmas Day. Re-Opening on Tuesday. Merry Christmas & Happy Holidays!
+                            We will be closed 1/13/23 - 1/19/23. Re-opening 1/20/23. 
                             <span role="img" aria-label="peace">
-                            ☃️🎄
+                            ✌️☮️
                             </span>
                             </p>
                         </div>
-                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://media1.giphy.com/media/uhywdQKtrYzx7GBreZ/giphy.gif?cid=ecf05e478q7vy6fwwjporhe5jwchdnpsp1k3niq6bg744vcg&rid=giphy.gif&ct=g)", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
+                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/325298192_731774871478719_7746555048476508817_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=fj3CXDdDFEkAX_x4Y07&_nc_ht=scontent-lga3-2.xx&oh=00_AfAN9sDw2BwEMfpanUJEkcwL4pfxA7vGHDyksEpxrVlizw&oe=63C5DC9C)", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
                         </div>
                         {/* <div style={{height: '30%', width: '100px', marginBottom: '10px'}}>
                             <a href="https://morethanthecurve.com/best-of-conshy/" style={{textDecoration: 'none', color: 'white'}}><button className="__button" style={{bottom: '5px'}}>Vote Now</button></a>
