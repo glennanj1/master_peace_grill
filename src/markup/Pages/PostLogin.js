9937396f8fdfa229bd61380f1f48504c95181ffa
Brooke Ross <brooke.ross2800@gmail.com>
2023-02-16 10:45:03 -0700

    postLoginComponent edited, CSS added, Buttons Edited, and Hours CSS edited

--- src/markup/Pages/PostLogin.js
+++ src/markup/Pages/PostLogin.js
@@ -6,12 +6,12 @@ import Header2 from '../Layout/Header2.js'
 function PostLogin({user}) {
 
     return (
-        <div className="post-login-page" style={{backgroundImage: "url(https://images.unsplash.com/photo-1533035353720-f1c6a75cd8ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)", backgroundSize: "cover"}} >
+        <div className="post-login-page" style={{backgroundImage: "url(https://d3ddatyom1hv87.cloudfront.net/background.jpg)", backgroundSize: "cover"}} >
             <Header2 />
             <div className="post-login-header">
                 <h3>Welcome, {user.email}</h3>
             </div>
-            <div className="buttons-parent jc-center">
+            <div className="buttons-parent">
                 <button class=" margin-right">Edit Menu</button>
                 <button>Edit Modal</button>
             </div>
