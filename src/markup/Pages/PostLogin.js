528ea6b3edbb4184a9149d7c9e37ea5a89744e20
glennanj1 <glennan20@gmail.com>
2023-02-18 23:50:21 -0500

    post login cleanup, removed link added conditional rendering to keep user context

--- src/markup/Pages/PostLogin.js
+++ src/markup/Pages/PostLogin.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import '../../css/login.css'
 import Header2 from '../Layout/Header2.js'
-import { Link } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
-
-function PostLogin({user}) {
+import { UserContext } from './../../context/UserContext';
+import MenuEditForm from './MenuComponentsFolder/MenuEditForm';
+function PostLogin() {
     let history = useHistory();
-
+    const { user } = useContext(UserContext);  
     !user ? history.push('/login') : console.log("welcome");
+    
+    const [editMenu, setEditMenu] = useState(false);
 
+    const handleEditMenu = () => {
+        setEditMenu(true);
+        console.log(editMenu);
+    }
+
+    if (editMenu) return <MenuEditForm /> 
+    
     return (
         <div className="post-login-page" style={{backgroundImage: "url(https://d3ddatyom1hv87.cloudfront.net/background.jpg)", backgroundSize: "cover"}} >
             <Header2 />
@@ -16,9 +25,7 @@ function PostLogin({user}) {
                 <h3>Welcome, {user.email}</h3>
             </div>
             <div className="buttons-parent">
-                <Link to={'/menu/edit'}>
-                    <button className="margin-right">Edit Menu</button>
-				</Link>
+                <button className="margin-right" onClick={handleEditMenu}>Edit Menu</button>
                 <button disabled='true'>Edit Modal</button>
             </div>
         </div>
