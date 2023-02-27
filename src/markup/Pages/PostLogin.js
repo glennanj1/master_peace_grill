981d2ec4d9f2ffa332d12bc7ca3a0dc9a0893209
glennanj1 <glennan20@gmail.com>
2023-02-26 20:17:33 -0500

    post login update

--- src/markup/Pages/PostLogin.js
+++ src/markup/Pages/PostLogin.js
@@ -1,33 +1,26 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import '../../css/login.css'
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from './../../context/UserContext';
-import MenuEditForm from './MenuComponentsFolder/MenuEditForm';
 import LogoutButton from '../Element/LogoutButton';
 
 function PostLogin() {
-    let history = useHistory();
+    let history = useNavigate();
     const { user } = useContext(UserContext);
-    !user ? history.push('/login') : console.log("welcome");
 
-    const [editMenu, setEditMenu] = useState(false);
-
-    const handleEditMenu = () => {
-        setEditMenu(true);
-        console.log('EditMenu clg on postLogin', editMenu);
-    }
-
-    if (editMenu) return <MenuEditForm setEditMenu={setEditMenu} />
-
+    useEffect(() => {
+        !user ? history('/login') : console.log("welcome");
+    }, [user, history])
+    
     return (
         <div className="post-login-page" style={{ backgroundImage: "url(https://d3ddatyom1hv87.cloudfront.net/background.jpg)", backgroundSize: "cover" }} >
             <div className="post-login-header">
-                <p>Welcome, {user.email}</p>
+                <p>Welcome, {user?.email}</p>
             </div>
             <div className="buttons-parent">
-                <button className="margin-right" onClick={handleEditMenu}>Edit Menu</button>
-                <button className="margin-right" disabled={true}>Edit Modal</button>
-                <button className="margin-right" onClick={() => history.push('/menu')}>See Menu</button>
+                <button className="margin-right" onClick={() => history('/change/menu/')}>Edit Menu</button>
+                <button className="margin-right" disabled="true">Edit Modal</button>
+                <button className="margin-right" onClick={() => history('/menu')}>See Menu</button>
                 <LogoutButton />
             </div>
         </div>
