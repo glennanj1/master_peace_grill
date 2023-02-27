afd649d85dd09e423087375d9d9fa8fe77e31955
Anna Schmidt <marshall1697@gmail.com>
2023-02-27 10:00:54 -0700

    added toast message for success and error w/ menu item update

--- src/markup/Pages/MenuComponentsFolder/MenuItemEdit.js
+++ src/markup/Pages/MenuComponentsFolder/MenuItemEdit.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import toast, { Toaster } from 'react-hot-toast';
 
 const MenuItemEdit = ({ item, category }) => {
 	const history = useNavigate();
@@ -12,7 +12,8 @@ const MenuItemEdit = ({ item, category }) => {
 		add_ons: item.add_ons ? item.add_ons : '',
 	};
 	const [formData, setFormData] = useState(initialState);
-	// const [errors, setErrors] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [errors, setErrors] = useState('');
 
 	// get values from user input/changes to form
 	const handleOnChange = (e) => {
@@ -25,41 +26,52 @@ const MenuItemEdit = ({ item, category }) => {
 		});
 	};
 
-    //handle submitting any changes to each item form
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //send this form data in a PATCH request to update values in DB
-        console.log('form-data', formData) 
+	//custom toast for successful update
+	const successNotify = () => toast.success('Sucess! Menu item updated.');
+
+	//custom toast for error w/ update
+	const errorNotify = () =>
+		toast.error(`Error ${errors}. This item was not updated.`);
+
+	//handle submitting any changes to each item form
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		//send this form data in a PATCH request to update values in DB
+		setLoading(true);
 
 		//development fetch req
+		// fetch(`https://master-peace-grill-backend.herokuapp.com/foods/${item.id}`
 		// fetch(` http://localhost:3000/foods/${item.id}`
-		
-        fetch(`https://master-peace-grill-backend.herokuapp.com/${item.id}`, {
-            method: "PATCH",
-			credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData)
-        })
-        .then((res) => {
-          if (res.ok) {
-            res.json().then((data) => {
-				console.log('data after patch',data)
-				
-            });
-          } else {
-            res.json().then((err) => {
-				console.log('ERROR from PATCH', err)
-				// setErrors(err.errors)
-				history('/login');
-			})
-          }
-        })
-    }
-	
+
+		fetch(`https://master-peace-grill-backend.herokuapp.com/foods/${item.id}`, {
+			method: 'PATCH',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(formData),
+		}).then((res) => {
+			if (res.ok) {
+				res.json().then((data) => {
+					setLoading(false);
+					setFormData(data);
+					successNotify();
+				});
+			} else {
+				res.json().then((err) => {
+					setLoading(false);
+					console.log('ERROR from PATCH', err);
+					setErrors(err.errors);
+					errorNotify();
+					history('/login');
+				});
+			}
+		});
+	};
+
 	return (
-		<div className='form-container'>
+		<div className="form-container">
+			<Toaster/>
 			<form className="item-edit-form" onSubmit={handleSubmit}>
 				<div className="single-item-input">
 					<label className="form-label">Menu Item</label>
@@ -86,8 +98,15 @@ const MenuItemEdit = ({ item, category }) => {
 				</div>
 				<div className="single-item-input">
 					<label className="form-label">Category</label>
-					<select id="category" name="category_id" value={formData.category_id} onChange={handleOnChange} required>
-                	<option label={category?.name} value={category?.id}>{category?.name}</option>
+					<select
+						id="category"
+						name="category_id"
+						value={formData.category_id}
+						onChange={handleOnChange}
+						required>
+						<option label={category?.name} value={category?.id}>
+							{category?.name}
+						</option>
 					</select>
 				</div>
 
@@ -113,9 +132,8 @@ const MenuItemEdit = ({ item, category }) => {
 						onChange={handleOnChange}
 					/>
 				</div>
-
 				<button className="submit-btn btn red" type="submit">
-					Submit Changes
+					{loading ? 'Loading...' : 'Submit Changes'}
 				</button>
 			</form>
 		</div>
