1478ec929d1697b36b3a017d08c88f7a4ebd0505
glennanj1 <glennan20@gmail.com>
2023-02-26 20:17:05 -0500

    refactor

--- src/markup/Pages/Menu.js
+++ src/markup/Pages/Menu.js
@@ -6,17 +6,6 @@ import MenuCategory from "./MenuComponentsFolder/MenuCategory";
 import MenuCategoryAndBYO from "./MenuComponentsFolder/MenuCategoryAndBYO";
 
 import bg from "./../../images/background/rainbow.jpeg";
-import app from "./../../images/menu/app.jpg";
-import wings from "./../../images/menu/wings.jpg";
-import salad from "./../../images/menu/salad.jpg";
-import wrap from "./../../images/menu/wrap.jpg";
-import club from "./../../images/menu/club1.jpg";
-import steak from "./../../images/menu/steak.jpg";
-import burger from "./../../images/menu/burger.jpg";
-import hoagie from "./../../images/menu/hoagie.jpg";
-import sandwich from "./../../images/menu/sandwhich.jpg";
-import grilledCheese from "./../../images/food/grilled_cheese.jpg";
-import catering from "./../../images/menu/catering.jpg";
 
 const onlineOrdering = "https://onlineordering.rmpos.com/Order/?wci=54MBz6OB";
 const fb =
@@ -35,7 +24,7 @@ function Menu() {
   async function fetchCurrentMenu() {
     try {
       let res = await fetch(
-        "https://master-peace-grill-backend.herokuapp.com/foods"
+        "http://localhost:3000/foods"
       );
       // for development run w/ ruby backend
       // let res = await fetch('http://localhost:3000/foods');
@@ -53,58 +42,24 @@ function Menu() {
       console.error("ERROR:", error);
     }
   }
+  // categories
+  const categories = [];
+  const foods = [];
 
-  //filter menu categories to pass into each component
-  //APPETIZERS
-  const appMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Appetizers") ? item : false
-  );
-  //WINGS
-  const wingMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Wings") ? item : false
-  );
-  //SALADS
-  const saladMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Salads") ? item : false
-  );
-  //WRAPS
-  const wrapMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Wraps") ? item : false
-  );
-  // //TripleDeckerClubs
-  const clubMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Triple") ? item : false
-  );
-  //CheeseSteaks
-  const cheeseSteakMenu = menuInfo.filter((item) =>
-    item.category.name.includes("CheeseSteak") ? item : false
-  );
-  // //Burgers
-  const burgerMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Burgers") ? item : false
-  );
 
-  ////// CAN UNCOMMENT THESE AND THE CORRESPONDING JSX COMPONENTS ONCE DB HAS FILLED DATA FOR THESE ITEMS
-  // // //Hoagies and Grinders
-  const hoagieAndGrinderMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Hoagies") ? item : false
-  );
-
-  // // //Sandwichees
-  const sandwichesMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Sandwiches") ? item : false
-  );
-
-  // // //Grilled Cheese
-  const grilledCheeseMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Grilled") ? item : false
-  );
-
-  // // //Catering 
-  const cateringMenu = menuInfo.filter((item) =>
-    item.category.name.includes("Catering") ? item : false
-  );
-
+  // get each category and put it into array
+  for (const item in menuInfo){
+    if (item === 'categories') {
+      menuInfo[item].map(c => {
+         return categories.push(c)
+     }) 
+    } else if (item === 'foods') {
+      menuInfo[item].map(f => {
+        return foods.push(f)
+      }) 
+    }
+  }
+  
   return (
     <div>
       <Header2 facebook={fb} yelp={yelp} online={onlineOrdering} />
@@ -132,36 +87,14 @@ function Menu() {
         </div>
 
         <div className="section-full bg-white">
-          <MenuCategory menu={appMenu} menuImg={app} cssClass={true} />
-          <MenuCategory menu={wingMenu} menuImg={wings} />
-          <MenuCategory menu={saladMenu} menuImg={salad} cssClass={true} />
-
-          <MenuCategoryAndBYO menu={wrapMenu} menuImg={wrap} />
-          <MenuCategoryAndBYO menu={clubMenu} menuImg={club} cssClass={true} />
-          <MenuCategoryAndBYO menu={cheeseSteakMenu} menuImg={steak} />
-          <MenuCategoryAndBYO
-            menu={burgerMenu}
-            menuImg={burger}
-            cssClass={true}
-          />
-          <MenuCategoryAndBYO
-            menu={hoagieAndGrinderMenu}
-            menuImg={hoagie}
-          />
-          <MenuCategoryAndBYO
-            menu={sandwichesMenu}
-            menuImg={sandwich}
-            cssClass={true}
-          />
-          <MenuCategoryAndBYO
-            menu={grilledCheeseMenu}
-            menuImg={grilledCheese}
-          />
-          <MenuCategory
-            menu={cateringMenu}
-            menuImg={catering}
-            cssClass={true}
-          />
+          {categories.map(c => {
+            if (c.menuCategory) {
+              return <MenuCategory menu={foods.filter(f => f.category_id === c.id)} menuImg={c.menuImage} cssClass={c.cssClass} category={c} />
+            } else if (c.menuCategoryAndBYO) {
+              return <MenuCategoryAndBYO menu={foods.filter(f => f.category_id === c.id)} menuImg={c.menuImage} cssClass={c.cssClass} category={c} />
+            }
+            return null;
+          })}
         </div>
       </div>
 
