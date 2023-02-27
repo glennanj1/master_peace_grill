86ac76c7db4632d246ab4373ea432531d86c27e4
glennanj1 <glennan20@gmail.com>
2023-02-26 20:15:36 -0500

    react router layout

--- src/markup/Markup.js
+++ src/markup/Markup.js
@@ -10,27 +10,22 @@ import PostLogin from './Pages/PostLogin';
 import { UserProvider } from '../context/UserContext';
 import MenuEditForm from './Pages/MenuComponentsFolder/MenuEditForm';
 
-
 function Markup() {
-
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route index path='/' exact element={ <Index2 />} />
-
 				<Route path='/about' exact element={ <About />} />
-
 				<Route path='/menu' exact element={ <Menu />} />
 
-
+				{/* Utilizing Layouts with React Router Dom 6 */}
 				<Route element={<UserProvider />}>
 					<Route path='/login' exact element={ <Login /> } />
 					<Route path="/change/home" element={<PostLogin/>} />
 					<Route path="/change/menu" element={<MenuEditForm/>} />
 				</Route>
-
+				{/* Need to fix this */}
 				<Route element={ <NotFound />} />
-
 			</Routes>
 		</BrowserRouter>
 	)
