1436c7a1ce318969411a227e2df12b46c8beb50a
glennanj1 <glennan20@gmail.com>
2023-03-05 16:03:42 -0500

    updated modal logic

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -1,65 +1,85 @@
-import confetti from "canvas-confetti";
-import React from "react";
+import React, {useState, useEffect} from "react";
+// import confetti from "canvas-confetti";
 // import steak from "../../images/steak_tray.jpg"
 
 // const background = 'https://media.giphy.com/media/K9i1AnJdj2kUfay0Rm/giphy.gif'
 
-export default class Modal extends React.Component {
+export default function Modal() {
 
-    state = {
-        isOpen: false
-    }
+    useEffect(() => {
+         fetchModal();
+    }, [])
 
-    toggleModal = () => {
-        if (this.state.isOpen) {
-            this.setState({isOpen: false});
+    const [isOpen, setIsOpen] = useState(true);
+    const [modalInfo, setModalInfo] = useState();
+    
+
+    let toggleModal = () => {
+        if (isOpen) {
+            setIsOpen(false);
         } else {
-        this.setState({
-            isOpen: true
-        });
+            setIsOpen(true);
         }
     }
 
-    confettiLaunch = () => {
-        confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 },
-            zIndex: 100000,
-          });
+    // let confettiLaunch = () => {
+    //     confetti({
+    //         particleCount: 100,
+    //         spread: 70,
+    //         origin: { y: 0.6 },
+    //         zIndex: 100000,
+    //     });
+    // }
+
+    async function fetchModal() {
+        try {
+          let res = await fetch(
+            "https://master-peace-grill-backend.herokuapp.com/modals"
+          );
+          // for development run w/ ruby backend
+          // let res = await fetch('http://localhost:3000/foods');
+    
+          if (!res.ok) {
+            const message = `Error occurred: ${res.status}`;
+            throw new Error(message);
+          }
+    
+          //set menu info w/ response from DB
+          const modalData = await res.json();
+          setModalInfo(modalData?.modals);
+        } catch (error) {
+          console.error("ERROR:", error);
+        }
     }
+    
+   if (isOpen) {
+       return (
+           <div>
+               {modalInfo?.active ? (
+                   <div className="__wrapper">
+                       {/* <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}> */}
+                       <div className="__child" style={{borderRadius: '10px', background: 'white'}}>
+                           <div>
+                               <p style={{fontSize: '1.5em', padding: '20px'}}>
+                               {modalInfo?.body}
+                               <span role="img" aria-label="peace">
+                               ✌️☮️
+                               </span>
+                               </p>
+                           </div>
+                           <div style={{height: '100%', width: '100%', backgroundImage: `url(${modalInfo?.picture})`, backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
+                           </div>
+                           {/* <div style={{height: '30%', width: '100px', marginBottom: '10px'}}>
+                               <a href="https://morethanthecurve.com/best-of-conshy/" style={{textDecoration: 'none', color: 'white'}}><button className="__button" style={{bottom: '5px'}}>Vote Now</button></a>
+                           </div> */}
+                           <div style={{height: '30%', width: '100px', marginBottom: '20px'}}>
+                               <button className="__button" onClick={toggleModal} style={{top: '5px', position: 'relative'}}>Close</button>
+                           </div>
+                       </div>
+                   </div> ) : null}      
+           </div>
+       )
+   }
+    
 
-
-    componentDidMount() {
-        this.toggleModal();
-    }
-
-  render() {
-
-    return (
-        <div>
-            {this.state.isOpen ? (
-                <div className="__wrapper">
-                    {/* <div className="__child" style={{background: `url(${background}) white`, backgroundSize: 'cover', backgroundPosition: 'center'}}> */}
-                    <div className="__child" style={{borderRadius: '10px', background: 'white'}}>
-                        <div>
-                            <p style={{fontSize: '1.5em', padding: '20px'}}>
-                            We will be closed 1/13/23 - 1/19/23. Re-opening 1/20/23. 
-                            <span role="img" aria-label="peace">
-                            ✌️☮️
-                            </span>
-                            </p>
-                        </div>
-                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/325298192_731774871478719_7746555048476508817_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=fj3CXDdDFEkAX_x4Y07&_nc_ht=scontent-lga3-2.xx&oh=00_AfAN9sDw2BwEMfpanUJEkcwL4pfxA7vGHDyksEpxrVlizw&oe=63C5DC9C)", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
-                        </div>
-                        {/* <div style={{height: '30%', width: '100px', marginBottom: '10px'}}>
-                            <a href="https://morethanthecurve.com/best-of-conshy/" style={{textDecoration: 'none', color: 'white'}}><button className="__button" style={{bottom: '5px'}}>Vote Now</button></a>
-                        </div> */}
-                        <div style={{height: '30%', width: '100px', marginBottom: '20px'}}>
-                            <button className="__button" onClick={this.toggleModal} style={{top: '5px', position: 'relative'}}>Close</button>
-                        </div>
-                    </div>
-                </div> ) : null}      
-        </div>
-    )}
 }
