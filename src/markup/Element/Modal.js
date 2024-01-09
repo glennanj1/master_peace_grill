7fbec62a84315f534afbc573b71c9741fb9f83f2
glennanj1 <glennan20@gmail.com>
2024-01-08 23:29:12 -0500

    modal update + package updates

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -1,7 +1,7 @@
 import confetti from "canvas-confetti";
 import React from "react";
 // import steak from "../../images/steak_tray.jpg"
-
+// import logo from './../../images/mpg_logo.png'
 // const background = 'https://media.giphy.com/media/K9i1AnJdj2kUfay0Rm/giphy.gif'
 
 export default class Modal extends React.Component {
@@ -44,13 +44,13 @@ export default class Modal extends React.Component {
                     <div className="__child" style={{borderRadius: '10px', background: 'white'}}>
                         <div>
                             <p style={{fontSize: '1.5em', padding: '20px'}}>
-                            We will be closed 1/13/23 - 1/19/23. Re-opening 1/20/23. 
+                            We will be closed 1/11/24 - 1/17/24. Re-opening 1/18/24. 
                             <span role="img" aria-label="peace">
                             ✌️☮️
                             </span>
                             </p>
                         </div>
-                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/325298192_731774871478719_7746555048476508817_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=fj3CXDdDFEkAX_x4Y07&_nc_ht=scontent-lga3-2.xx&oh=00_AfAN9sDw2BwEMfpanUJEkcwL4pfxA7vGHDyksEpxrVlizw&oe=63C5DC9C)", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
+                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://imagedelivery.net/VQh5-_xRHTXIspD07tbGiw/dec76d61-b2f5-46bb-863c-5afd3dde9300/public", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
                         </div>
                         {/* <div style={{height: '30%', width: '100px', marginBottom: '10px'}}>
                             <a href="https://morethanthecurve.com/best-of-conshy/" style={{textDecoration: 'none', color: 'white'}}><button className="__button" style={{bottom: '5px'}}>Vote Now</button></a>
@@ -58,6 +58,7 @@ export default class Modal extends React.Component {
                         <div style={{height: '30%', width: '100px', marginBottom: '20px'}}>
                             <button className="__button" onClick={this.toggleModal} style={{top: '5px', position: 'relative'}}>Close</button>
                         </div>
+                        {/* </div> */}
                     </div>
                 </div> ) : null}      
         </div>
