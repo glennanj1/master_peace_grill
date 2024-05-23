f66c9f02a47f6bdbb9b21116c7a31b4809071bc6
John Glennan <61764500+glennanj1@users.noreply.github.com>
2024-05-22 22:15:07 -0400

    modal push

--- src/markup/Element/Modal.js
+++ src/markup/Element/Modal.js
@@ -44,13 +44,13 @@ export default class Modal extends React.Component {
                     <div className="__child" style={{borderRadius: '10px', background: 'white'}}>
                         <div>
                             <p style={{fontSize: '1.5em', padding: '20px'}}>
-                            We will be closed 1/11/24 - 1/17/24. Re-opening 1/19/24. 
+                            Masterpeace Grill will be closed Friday through Wednesday
                             <span role="img" aria-label="peace">
-                            ✌️☮️
+                            ✌️☮️🎶🎶🎶🎶🎶🎶🎶🎶
                             </span>
                             </p>
                         </div>
-                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://imagedelivery.net/VQh5-_xRHTXIspD07tbGiw/dec76d61-b2f5-46bb-863c-5afd3dde9300/public", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
+                        <div style={{height: '100%', width: '100%', backgroundImage: "url(https://scontent-mia3-1.xx.fbcdn.net/v/t39.30808-6/436275260_926074752863111_8295943386381515439_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=7KqOluyciBsQ7kNvgFqzaAK&_nc_ht=scontent-mia3-1.xx&oh=00_AYBmL7vNKda8VrsR3c1eZsCtDQH45GeVx6msAZwufRsapg&oe=66545F22", backgroundSize: 'contain', backgroundPosition: 'center', marginBottom: '10px'}}>
                         </div>
                         {/* <div style={{height: '30%', width: '100px', marginBottom: '10px'}}>
                             <a href="https://morethanthecurve.com/best-of-conshy/" style={{textDecoration: 'none', color: 'white'}}><button className="__button" style={{bottom: '5px'}}>Vote Now</button></a>
